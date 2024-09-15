db7dce9b94f51426fb5af385a3e866bb8c4af572
CGAJAY <jmugendi737@gmail.com>
2024-09-15 19:54:25 +0300

    Created a card with photo, title and description

---
+++ src/Card.jsx
@@ -0,0 +1,18 @@
+import "./card.css";
+const Card = () => {
+	const photo =
+		"https://avatars.githubusercontent.com/u/149392018?s=400&u=d3aeb371064a686556a61983f5202bab15163a28&v=4";
+	return (
+		<div className="card">
+			<img
+				src={photo}
+				alt="My 3d anime"
+				className="photo"
+			/>
+			<h2 className="title">Dev Ajay</h2>
+			<p className="desc">I create Websites</p>
+		</div>
+	);
+};
+
+export default Card;
--- src/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-	return (
-		<footer>
-			<p>&copy; {new Date().getFullYear()} devajay.me</p>
-		</footer>
-	);
-};
-
-export default Footer;
